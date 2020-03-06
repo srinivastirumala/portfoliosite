a9bf70233350a41563b6f7e26c64bdb6b76a8506
srinivast <srinivast.42@gmail.com>
2020-03-06 14:17:22 +0400

    Updates

---
+++ precache-manifest.46049ee2eab4d84d2adbb5e152fab5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "90f16b2370dce1d6a144",
+    "url": "/portfoliosite/static/js/runtime~main.90f16b23.js"
+  },
+  {
+    "revision": "f8f6231079ebb7d93d28",
+    "url": "/portfoliosite/static/js/main.f8f62310.chunk.js"
+  },
+  {
+    "revision": "6f33b68aa0a123bb6fbd",
+    "url": "/portfoliosite/static/js/1.6f33b68a.chunk.js"
+  },
+  {
+    "revision": "f8f6231079ebb7d93d28",
+    "url": "/portfoliosite/static/css/main.c0cb615b.chunk.css"
+  },
+  {
+    "revision": "6f33b68aa0a123bb6fbd",
+    "url": "/portfoliosite/static/css/1.53e2d71f.chunk.css"
+  },
+  {
+    "revision": "900eafc430346da4e969139377587a39",
+    "url": "/portfoliosite/index.html"
+  }
+];
